24a287b72135ea58d6ef10e88587278b25599c70
yvan99 <ishimweyvan90@gmail.com>
2023-02-25 20:42:38 +0200

    finalize rra seeder

--- database/seeders/RraSeeder.php
+++ database/seeders/RraSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Rra;
 use Illuminate\Database\Seeder;
 
 class RraSeeder extends Seeder
@@ -13,6 +14,8 @@ class RraSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Rra::factory()
+            ->count(10)
+            ->create();
     }
 }
