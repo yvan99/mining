452553d8c8ba55a72875b1baba74fc0038626fef
yvan99 <ishimweyvan90@gmail.com>
2023-02-25 20:39:14 +0200

    finalize rra factory

--- database/factories/RraFactory.php
+++ database/factories/RraFactory.php
@@ -2,19 +2,21 @@
 
 namespace Database\Factories\Models;
 
+use App\Models\Rra;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use Illuminate\Support\Facades\Hash;
 class RraFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+    protected $model = Rra::class;
+
     public function definition()
     {
         return [
-            //
+            'names' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('password'), // default password
+            'status' => $this->faker->randomElement(['active', 'inactive']),
+            'ra-code' => $this->faker->unique()->randomNumber(),
         ];
     }
 }
