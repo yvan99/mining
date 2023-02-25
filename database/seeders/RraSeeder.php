bc11e35a1234406755c1a1314c9cdfd2c92e5b09
yvan99 <ishimweyvan90@gmail.com>
2023-02-25 20:35:56 +0200

    create rra seeder

---
+++ database/seeders/RraSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class RraSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
