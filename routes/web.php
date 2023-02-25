ca878a6eb333042a795feca6455bcfa20a4ba39d
yvan99 <ishimweyvan90@gmail.com>
2023-02-25 22:11:18 +0200

    fix auth view routes

--- routes/web.php
+++ routes/web.php
@@ -22,21 +22,21 @@ Route::get('/', function () {
 
 // Admin routes
 Route::prefix('admin')->group(function () {
-    Route::get('/login', 'admin.login')->name('admin.login');
+    Route::view('/login', 'admin.login')->name('admin.login');
     Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.login.submit');
     Route::post('/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
 });
 
 // Rra routes
 Route::prefix('rra')->group(function () {
-    Route::get('/login', 'rra.login')->name('rra.login');
+    Route::view('/login', 'rra.login')->name('rra.login');
     Route::post('/login', [RraAuthController::class, 'login'])->name('rra.login.submit');
     Route::post('/logout', [RraAuthController::class, 'logout'])->name('rra.logout');
 });
 
 // Client routes
 Route::prefix('client')->group(function () {
-    Route::get('/login', 'client.login')->name('client.login');
+    Route::view('/login', 'client.login')->name('client.login');
     Route::post('/login', [ClientAuthController::class, 'login'])->name('client.login.submit');
     Route::post('/logout', [ClientAuthController::class, 'logout'])->name('client.logout');
 });
