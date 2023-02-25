cf6f39021ad58b128d0cbf4b13fb594dd8de5045
yvan99 <ishimweyvan90@gmail.com>
2023-02-25 20:26:52 +0200

    admin fields

--- database/migrations/2023_02_25_182450_create_admins_table.php
+++ database/migrations/2023_02_25_182450_create_admins_table.php
@@ -15,8 +15,12 @@ class CreateAdminsTable extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->timestamps();
         });
+        
     }
 
     /**
