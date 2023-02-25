dd94edbe89f032cf23bcd487b62a98a7063c5f95
yvan99 <ishimweyvan90@gmail.com>
2023-02-25 21:47:49 +0200

    rrauth auth

---
+++ app/Http/Controllers/RraAuthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rra;
+use Illuminate\Http\Request;
+
+class RraAuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Rra  $rra
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Rra $rra)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Rra  $rra
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Rra $rra)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Rra  $rra
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Rra $rra)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Rra  $rra
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Rra $rra)
+    {
+        //
+    }
+}
