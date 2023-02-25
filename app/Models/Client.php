54fdf97a857d875cc97b3d5b7cca9a5851c127f9
yvan99 <ishimweyvan90@gmail.com>
2023-02-25 23:37:53 +0200

    client model authenticable

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Client extends Model
+class Client extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password', 'address', 'phone', 'cli_code', 'status',
+    ];
 }
