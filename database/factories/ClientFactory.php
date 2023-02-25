3bbb9263a5d6a17a0b30f176ee9dcf1ee2c375cf
yvan99 <ishimweyvan90@gmail.com>
2023-02-25 20:40:50 +0200

    finalize client factory

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -2,19 +2,23 @@
 
 namespace Database\Factories\Models;
 
+use App\Models\Client;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use Illuminate\Support\Facades\Hash;
 class ClientFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+    protected $model = Client::class;
+
     public function definition()
     {
         return [
-            //
+            'names' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('password'), // default password
+            'address' => $this->faker->address,
+            'phone' => $this->faker->phoneNumber,
+            'cli-code' => $this->faker->unique()->randomNumber(),
+            'status' => $this->faker->randomElement(['active', 'inactive']),
         ];
     }
 }
