9d5aee1757f7255d03508a9757d350b4eb13a0d1
yvan99 <ishimweyvan90@gmail.com>
2023-02-25 20:38:19 +0200

    finalize admin factory

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -2,19 +2,19 @@
 
 namespace Database\Factories\Models;
 
+use App\Models\Admin;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use Illuminate\Support\Facades\Hash;
 class AdminFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+    protected $model = Admin::class;
+
     public function definition()
     {
         return [
-            //
+            'names' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('password'), // default password
         ];
     }
 }
