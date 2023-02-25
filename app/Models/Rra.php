7cbf2358fcf247201c866c9cc30588b8b98a4128
yvan99 <ishimweyvan90@gmail.com>
2023-02-25 23:36:42 +0200

    rra model authenticable

--- app/Models/Rra.php
+++ app/Models/Rra.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Rra extends Model
+class Rra extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password', 'status', 'ra_code',
+    ];
+
 }
