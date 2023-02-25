be0da8ad6bf58d2333962dc5ae140b1ee3b21183
yvan99 <ishimweyvan90@gmail.com>
2023-02-25 20:34:30 +0200

    create admin factory

---
+++ database/factories/AdminFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories\Models;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AdminFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
