44c6b86ddc41d4ff53c1551491994ef29c3a8755
yvan99 <ishimweyvan90@gmail.com>
2023-02-25 23:28:28 +0200

    show login errors

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -14,24 +14,41 @@
                                         <h6>Sign in {{ env('APP_NAME') }}</h6>
                                     </div>
                                 </div>
-                                <div class="card-body">
-                                    <div class="edit-profile__body">
-                                        <div class="form-group mb-25">
-                                            <label for="username">Email Address</label>
-                                            <input type="text" class="form-control" id="username"
-                                                placeholder="Valid Email Address">
-                                        </div>
-                                        <div class="form-group mb-15">
-                                            <label for="password-field">Password</label>
-                                            <div class="position-relative">
-                                                <input id="password-field" type="password" class="form-control"
-                                                    name="password" placeholder="*****">
-                                                <div
-                                                    class="uil uil-eye-slash text-lighten fs-15 field-icon toggle-password2">
-                                                </div>
+
+                                <form method="POST" action="{{ route('admin.login') }}">
+                                    @csrf
+                                    <div class="card-body">
+                                        <div class="edit-profile__body">
+                                            <div class="form-group mb-25">
+                                                <label for="username">Email Address</label>
+                                                <input type="text"
+                                                    class="form-control @error('email') is-invalid @enderror"
+                                                    name="email" value="{{ old('email') }}"
+                                                    placeholder="Valid Email Address">
+
+                                                @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
                                             </div>
-                                        </div>
-                                        {{-- <div class="admin-condition">
+                                            <div class="form-group mb-15">
+                                                <label for="password-field">Password</label>
+                                                <div class="position-relative">
+                                                    <input id="password-field" type="password" name="password" class="form-control @error('password') is-invalid @enderror"
+                                                        name="password" placeholder="*****">
+                                                    <div
+                                                        class="uil uil-eye-slash text-lighten fs-15 field-icon toggle-password2">
+                                                    </div>
+                                                </div>
+
+                                                @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                            </div>
+                                            {{-- <div class="admin-condition">
                                             <div class="checkbox-theme-default custom-checkbox ">
                                                 <input class="checkbox" type="checkbox" id="check-1">
                                                 <label for="check-1">
@@ -39,15 +56,16 @@
                                                 </label>
                                             </div>
                                         </div> --}}
-                                        <div
-                                            class="admin__button-group button-group d-flex pt-1 justify-content-md-start justify-content-center">
-                                            <button
-                                                class="btn btn-primary btn-default w-100 btn-squared text-capitalize lh-normal px-50 signIn-createBtn ">
-                                                Connect Account
-                                            </button>
+                                            <div
+                                                class="admin__button-group button-group d-flex pt-1 justify-content-md-start justify-content-center">
+                                                <button type="submit"
+                                                    class="btn btn-primary btn-default w-100 btn-squared text-capitalize lh-normal px-50 signIn-createBtn ">
+                                                    Connect Account
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
+                                </form>
                                 <div class="admin-topbar">
                                     <p class="mb-0">
                                         Don't have an account?
