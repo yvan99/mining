7153af55c34522ab154d701c66db3820f0677b63
yvan99 <ishimweyvan90@gmail.com>
2023-02-26 10:39:33 +0200

    finalize intouch sms method

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class SmsController extends Controller
 {
-    //
+    public function sendSms($receiver, $message)
+    {
+        $client = new Client();
+        $data = array(
+            "sender"     => env("INTOUCH_SENDER"),
+            "recipients" => $receiver,
+            "message"    => $message,
+        );
+
+        $url = env("INTOUCH_API");
+        $response = $client->request('POST', $url, [
+            'auth' => [env("INTOUCH_USERNAME"), env("INTOUCH_PASSWORD")],
+            'form_params' => $data,
+            'verify' => false,
+        ]);
+
+        $result = $response->getBody()->getContents();
+        $httpcode = $response->getStatusCode();
+        if ($httpcode != 200) {
+            # code...
+            return $httpcode . 'SMS system error ,SMS not send';
+            exit;
+        } else {
+            error_log($result);
+            return $result;
+        }
+    }
 }
