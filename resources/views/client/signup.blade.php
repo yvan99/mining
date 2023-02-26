4c1fadd5b3d8fbe973072ac3afb85d865936b1c6
yvan99 <ishimweyvan90@gmail.com>
2023-02-26 10:27:45 +0200

    fix signup blade vieew

--- resources/views/client/signup.blade.php
+++ resources/views/client/signup.blade.php
@@ -4,22 +4,38 @@
     <main class="main-content bg-dark">
         @include('components.loader')
         <div class="admin ">
-            <div class="container">
+            <div class="container-fluid">
                 <div class="row justify-content-center">
-                    <div class="col-xxl-3 col-xl-4 col-md-6 col-sm-8">
+                    <div class="col-xxl-3 col-xl-6 col-md-6 col-sm-8">
                         <div class="mt-5">
                             <div class="card border-1">
                                 <div class="card-header">
                                     <div class="edit-profile__title">
-                                        <h6>Sign in {{ env('APP_NAME') }}  : : Client Area</h6>
+                                        <h6>Create Account {{ env('APP_NAME') }}</h6>
                                     </div>
                                 </div>
 
-                                <form method="POST" action="{{ route('client.login') }}">
+                                <form method="POST" action="{{ route('client.signup') }}">
                                     @csrf
                                     <div class="card-body">
-                                        <div class="edit-profile__body">
-                                            <div class="form-group mb-25">
+                                        <div class="edit-profile__body row col-xl-12">
+
+                                          <div class="form-group mb-25 col-xl-6">
+                                            <label for="username">Full Names</label>
+                                            <input type="text"
+                                                class="form-control @error('name') is-invalid @enderror"
+                                                name="name" value="{{ old('name') }}"
+                                                placeholder="Ex:John Doe">
+
+                                            @error('name')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+
+
+                                            <div class="form-group mb-25 col-xl-6">
                                                 <label for="username">Email Address</label>
                                                 <input type="text"
                                                     class="form-control @error('email') is-invalid @enderror"
@@ -32,6 +48,36 @@
                                                     </span>
                                                 @enderror
                                             </div>
+
+                                            <div class="form-group mb-25 col-xl-6">
+                                              <label for="username">Telephone Number</label>
+                                              <input type="text"
+                                                  class="form-control @error('phone') is-invalid @enderror"
+                                                  name="phone" value="{{ old('phone') }}"
+                                                  placeholder="Ex:Valid Phone Number">
+  
+                                              @error('phone')
+                                                  <span class="invalid-feedback" role="alert">
+                                                      <strong>{{ $message }}</strong>
+                                                  </span>
+                                              @enderror
+                                          </div>
+
+
+                                          <div class="form-group mb-25 col-xl-6">
+                                            <label for="username">Residence Address</label>
+                                            <input type="text"
+                                                class="form-control @error('address') is-invalid @enderror"
+                                                name="address" value="{{ old('address') }}"
+                                                placeholder="Ex:Enter your address">
+
+                                            @error('address')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+
                                             <div class="form-group mb-15">
                                                 <label for="password-field">Password</label>
                                                 <div class="position-relative">
@@ -55,7 +101,7 @@
                                                 class="admin__button-group button-group d-flex pt-1 justify-content-md-start justify-content-center">
                                                 <button type="submit"
                                                     class="btn btn-primary btn-default w-100 btn-squared text-capitalize lh-normal px-50 signIn-createBtn ">
-                                                    Connect Account
+                                                    Create Account
                                                 </button>
                                             </div>
                                         </div>
