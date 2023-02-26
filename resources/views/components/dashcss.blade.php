f09d4324f07ab5d4b0e0610e45d936f9660effcc
yvan99 <ishimweyvan90@gmail.com>
2023-02-26 12:10:52 +0200

    add font

--- resources/views/components/dashcss.blade.php
+++ resources/views/components/dashcss.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mining Project</title>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="{{ asset('dashboard/css/plugin.min.css') }}">
     <link rel="stylesheet" href="{{ asset('dashboard/css/style.css') }}">
