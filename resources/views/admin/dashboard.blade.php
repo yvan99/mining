d0aec3152666860f6944bb53aa384e89a7b0ab75
yvan99 <ishimweyvan90@gmail.com>
2023-02-26 14:22:02 +0200

    remove mobile search

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,13 +1,7 @@
 @include('components.dashcss')
 
 <body class="layout-light side-menu">
-    <div class="mobile-search">
-        <form action="https://demo.dashboardmarket.com/" class="search-form">
-            <img src="img/svg/search.svg" alt="search" class="svg">
-            <input class="form-control me-sm-2 box-shadow-none" type="search" placeholder="Search..."
-                aria-label="Search">
-        </form>
-    </div>
+
     <div class="mobile-author-actions"></div>
     <header class="header-top">
         <nav class="navbar navbar-light">
