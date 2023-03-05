4061a28f593d2b2280aedfcd7fcb4a835d19a8e9
yvan99 <ishimweyvan90@gmail.com>
2023-03-05 16:44:41 +0200

    feature

---
+++ database/migrations/2023_03_05_144400_create_minerals_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMineralsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('minerals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('mine_tag');
+            $table->integer('quantity');
+            $table->string('picture')->nullable();
+            $table->text('content')->nullable();
+            $table->string('source')->nullable();
+            $table->float('weight');
+            $table->float('exported_value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('minerals');
+    }
+}
