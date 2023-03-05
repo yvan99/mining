f975797e013627fab1042d14ce787e033c4f3688
yvan99 <ishimweyvan90@gmail.com>
2023-03-05 16:43:57 +0200

    feature

--- resources/views/minerals/create.blade.php
+++ resources/views/minerals/create.blade.php
@@ -24,35 +24,178 @@
                                 </div>
                             </div>
                         </div>
-                        <form action="{{ route('minerals.store') }}" method="POST" enctype="multipart/form-data">
-                            @csrf
 
-                            <div>
-                                <label for="name">Name:</label>
-                                <input type="text" name="name" id="name" value="{{ old('name') }}"
-                                    required>
-                                @error('name')
-                                    <div>{{ $message }}</div>
-                                @enderror
+                            <div class="container">
+                                <div class="row justify-content-center">
+                                    <div class="col-md-12">
+                                        <div class="card">
+                                            <div class="card-header">{{ __('Add Mineral') }}</div>
+
+                                            <div class="card-body">
+                                                <form method="POST" action="{{ route('minerals.store') }}"
+                                                    enctype="multipart/form-data">
+                                                    @csrf
+
+                                                    <div class="form-group row">
+                                                        <label for="name"
+                                                            class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                                                        <div class="col-md-6">
+                                                            <input id="name" type="text"
+                                                                class="form-control @error('name') is-invalid @enderror"
+                                                                name="name" value="{{ old('name') }}" required
+                                                                autocomplete="name" autofocus>
+
+                                                            @error('name')
+                                                                <span class="invalid-feedback" role="alert">
+                                                                    <strong>{{ $message }}</strong>
+                                                                </span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group row">
+                                                        <label for="mine_tag"
+                                                            class="col-md-4 col-form-label text-md-right">{{ __('Mine Tag') }}</label>
+
+                                                        <div class="col-md-6">
+                                                            <input id="mine_tag" type="text"
+                                                                class="form-control @error('mine_tag') is-invalid @enderror"
+                                                                name="mine_tag" value="{{ old('mine_tag') }}" required
+                                                                autocomplete="mine_tag">
+
+                                                            @error('mine_tag')
+                                                                <span class="invalid-feedback" role="alert">
+                                                                    <strong>{{ $message }}</strong>
+                                                                </span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group row">
+                                                        <label for="quantity"
+                                                            class="col-md-4 col-form-label text-md-right">{{ __('Quantity') }}</label>
+
+                                                        <div class="col-md-6">
+                                                            <input id="quantity" type="number"
+                                                                class="form-control @error('quantity') is-invalid @enderror"
+                                                                name="quantity" value="{{ old('quantity') }}" required
+                                                                autocomplete="quantity">
+
+                                                            @error('quantity')
+                                                                <span class="invalid-feedback" role="alert">
+                                                                    <strong>{{ $message }}</strong>
+                                                                </span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group row">
+                                                        <label for="picture"
+                                                            class="col-md-4 col-form-label text-md-right">{{ __('Picture') }}</label>
+
+                                                        <div class="col-md-6">
+                                                            <input id="picture" type="file"
+                                                                class="form-control-file @error('picture') is-invalid @enderror"
+                                                                name="picture" accept="image/*">
+
+                                                            @error('picture')
+                                                                <span class="invalid-feedback" role="alert">
+                                                                    <strong>{{ $message }}</strong>
+                                                                </span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group row">
+                                                        <label for="content"
+                                                            class="col-md-4 col-form-label text-md-right">{{ __('Content') }}</label>
+
+                                                        <div class="col-md-6">
+                                                            <textarea id="content" class="form-control @error('content') is-invalid @enderror" name="content" rows="3">{{ old('content') }}</textarea>
+
+                                                            @error('content')
+                                                                <span class="invalid-feedback" role="alert">
+                                                                    <strong>{{ $message }}</strong>
+                                                                </span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group row">
+                                                        <label for="source"
+                                                            class="col-md-4 col-form-label text-md-right">{{ __('Source') }}</label>
+
+                                                        <div class="col-md-6">
+                                                            <input id="source" type="text"
+                                                                class="form-control @error('source') is-invalid @enderror"
+                                                                name="source" value="{{ old('source') }}" required
+                                                                autocomplete="source">
+
+                                                            @error('source')
+                                                                <span class="invalid-feedback" role="alert">
+                                                                    <strong>{{ $message }}</strong>
+                                                                </span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group row">
+                                                        <label for="weight"
+                                                            class="col-md-4 col-form-label text-md-right">{{ __('Weight (kg)') }}</label>
+
+                                                        <div class="col-md-6">
+                                                            <input id="weight" type="number" step="0.01"
+                                                                class="form-control @error('weight') is-invalid @enderror"
+                                                                name="weight" value="{{ old('weight') }}" required
+                                                                autocomplete="weight">
+
+                                                            @error('weight')
+                                                                <span class="invalid-feedback" role="alert">
+                                                                    <strong>{{ $message }}</strong>
+                                                                </span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group row">
+                                                        <label for="exported_value"
+                                                            class="col-md-4 col-form-label text-md-right">{{ __('Exported Value') }}</label>
+
+                                                        <div class="col-md-6">
+                                                            <input id="exported_value" type="number" step="0.01"
+                                                                class="form-control @error('exported_value') is-invalid @enderror"
+                                                                name="exported_value" value="{{ old('exported_value') }}"
+                                                                required autocomplete="exported_value">
+
+                                                            @error('exported_value')
+                                                                <span class="invalid-feedback" role="alert">
+                                                                    <strong>{{ $message }}</strong>
+                                                                </span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group row mb-0">
+                                                        <div class="col-md-6 offset-md-4">
+                                                            <button type="submit" class="btn btn-primary">
+                                                                {{ __('Save') }}
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
 
-                            <div>
-                                <label for="mine_tag">Mine Tag:</label>
-                                <input type="text" name="mine_tag" id="mine_tag" value="{{ old('mine_tag') }}"
-                                    required>
-                                @error('mine_tag')
-                                    <div>{{ $message }}</div>
-                                @enderror
-                            </div>
 
-                            <div>
-                                <label for="quantity">Quantity:</label>
-                                <input type="number" name="quantity" id="quantity" value="{{ old('quantity') }}">
-                            </div>
 
+
+                        </div>
                     </div>
                 </div>
-            </div>
-            @include('components.dashfooter')
-    </main>
-    @include('components.dashjs')
+                @include('components.dashfooter')
+        </main>
+        @include('components.dashjs')
--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ Route::prefix('client')->group(function () {
 Route::prefix('admin')->middleware(['auth:admin'])->group(function () {
     Route::view('/dashboard', 'admin.dashboard');
     Route::get('/new-mineral', [MineralController::class, 'create']);
-    Route::post('/minerals', 'MineralController@store')->name('minerals.store');
+    Route::post('/minerals', [MineralController::class, 'store'])->name('minerals.store');
 
 });
 
