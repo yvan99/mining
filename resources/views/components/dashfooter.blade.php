49a05167960290a313ef21052e7dd2ae7c2c7094
yvan99 <ishimweyvan90@gmail.com>
2023-03-05 15:28:26 +0200

    create dashboard footer

---
+++ resources/views/components/dashfooter.blade.php
@@ -0,0 +1,30 @@
+<footer class="footer-wrapper">
+  <div class="footer-wrapper__inside">
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-md-6">
+                  <div class="footer-copyright">
+                      <p><span>© </span><a href="#">Sovware</a>
+                      </p>
+                  </div>
+              </div>
+              <div class="col-md-6">
+                  <div class="footer-menu text-end">
+                      <ul>
+                          <li>
+                              <a href="#">About</a>
+                          </li>
+                          <li>
+                              <a href="#">Team</a>
+                          </li>
+                          <li>
+                              <a href="#">Contact</a>
+                          </li>
+                      </ul>
+                  </div>
+
+              </div>
+          </div>
+      </div>
+  </div>
+</footer>
