a7c5a411db38e51e90a97f59a0f28b57b1d520d4
yvan99 <ishimweyvan90@gmail.com>
2023-03-05 15:26:34 +0200

    make menus admin

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -5,24 +5,52 @@
               <li>
                   <a href="changelog.html" class="">
                       <span class="nav-icon uil uil-arrow-growth"></span>
-                      <span class="menu-text">Changelog</span>
-                      <span class="badge badge-info-10 menuItem rounded-pill">1.1.4</span>
+                      <span class="menu-text">Dashboard</span>
+                     
                   </a>
               </li>
 
               <li>
+                <a href="calendar.html" class="">
+                    <span class="nav-icon uil uil-calendar-alt"></span>
+                    <span class="menu-text">Register Minerals</span>
+                </a>
+            </li>
+
+              <li>
                   <a href="calendar.html" class="">
                       <span class="nav-icon uil uil-calendar-alt"></span>
-                      <span class="menu-text">Calendar</span>
+                      <span class="menu-text">Manage Minerals</span>
                   </a>
               </li>
  
               <li class="">
                   <a href="sign-up.html">
                       <span class="nav-icon uil uil-sign-out-alt"></span>
-                      <span class="menu-text">Sign Up</span>
+                      <span class="menu-text">Manage Orders</span>
                   </a>
               </li>
+
+              <li class="">
+                <a href="sign-up.html">
+                    <span class="nav-icon uil uil-sign-out-alt"></span>
+                    <span class="menu-text">RRA Inspections</span>
+                </a>
+            </li>
+
+              <li class="">
+                <a href="sign-up.html">
+                    <span class="nav-icon uil uil-sign-out-alt"></span>
+                    <span class="menu-text">Shipping Partners</span>
+                </a>
+            </li>
+
+            <li class="">
+                <a href="sign-up.html">
+                    <span class="nav-icon uil uil-sign-out-alt"></span>
+                    <span class="menu-text">Sign Out</span>
+                </a>
+            </li>
           </ul>
       </div>
   </div>
