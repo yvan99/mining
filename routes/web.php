0585464193bcceb58e016276318dc845017a6edb
yvan99 <ishimweyvan90@gmail.com>
2023-03-07 18:01:54 +0200

    show orders fx

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,6 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\RraAuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('client.login');
 });
