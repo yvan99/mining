52b5aac9eebba4fe241a4dcdb9e886834bda0fa6
yvan99 <ishimweyvan90@gmail.com>
2023-03-11 11:26:20 +0200

    feature

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -10,42 +10,42 @@
                   </a>
               </li>
 
-              <li>
+              <li class="{{ Request::is('admin/dashboard') ? 'active' : '' }}">
                 <a href="/admin/new-mineral">
                     <span class="nav-icon uil uil-plus-circle"></span>
                     <span class="menu-text">Register Minerals</span>
                 </a>
             </li>
 
-              <li>
+              <li class="{{ Request::is('admin/dashboard') ? 'active' : '' }}">
                   <a href="/admin/manage-minerals">
                       <span class="nav-icon uil uil-database"></span>
                       <span class="menu-text">Manage Minerals</span>
                   </a>
               </li>
  
-              <li>
+              <li class="{{ Request::is('admin/dashboard') ? 'active' : '' }}">
                   <a href="/admin/orders">
                       <span class="nav-icon uil uil-luggage-cart"></span>
                       <span class="menu-text">Manage Orders</span>
                   </a>
               </li>
 
-              <li>
+              <li class="{{ Request::is('admin/inspection') ? 'active' : '' }}">
                 <a href="/admin/inspection">
                     <span class="nav-icon uil uil-exchange"></span>
                     <span class="menu-text">RRA Inspections</span>
                 </a>
             </li>
 
-              <li>
+              <li class="{{ Request::is('admin/shipping') ? 'active' : '' }}">
                 <a href="/admin/shipping">
                     <span class="nav-icon uil uil-user"></span>
                     <span class="menu-text">Shipping Partners</span>
                 </a>
             </li>
 
-            <li>
+            <li class="{{ Request::is('admin/logout') ? 'active' : '' }}">
                 <a href="/admin/logout">
                     <span class="nav-icon uil uil-sign-out-alt"></span>
                     <span class="menu-text">Sign Out</span>
