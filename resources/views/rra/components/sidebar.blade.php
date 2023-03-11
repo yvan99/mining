29a7aa45a5c21166d37860320b7eb69058f44037
yvan99 <ishimweyvan90@gmail.com>
2023-03-11 13:00:17 +0200

    feature

--- resources/views/rra/components/sidebar.blade.php
+++ resources/views/rra/components/sidebar.blade.php
@@ -2,7 +2,7 @@
     <div class="sidebar sidebar-collapse" id="sidebar">
         <div class="sidebar__menu-group">
             <ul class="sidebar_nav">
-                <li>
+                <li class="{{ Request::is('rra/dashboard') ? 'active' : '' }}">
                     <a href="/rra/dashboard">
                         <span class="nav-icon uil uil-arrow-growth"></span>
                         <span class="menu-text">Dashboard</span>
@@ -10,14 +10,14 @@
                     </a>
                 </li>
 
-                <li>
+                <li class="{{ Request::is('rra/transit') ? 'active' : '' }}">
                     <a href="/rra/transit">
                         <span class="nav-icon uil uil-luggage-cart"></span>
                         <span class="menu-text">Transit Report</span>
                     </a>
                 </li>
 
-                <li>
+                <li class="{{ Request::is('rra/logout') ? 'active' : '' }}">
                     <a href="/rra/logout">
                         <span class="nav-icon uil uil-sign-out-alt"></span>
                         <span class="menu-text">Sign Out</span>
