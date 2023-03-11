949e7442dde4c054a8381bcd9e45299039a73a86
yvan99 <ishimweyvan90@gmail.com>
2023-03-11 17:31:38 +0200

    feature

---
+++ resources/views/components/invoicecss.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Invce - Invoice HTML Template - Template One</title>
+    <meta name="author" content="Angfuzsoft">
+    <meta name="description" content="Invce - Invoice HTML Template">
+    <meta name="keywords" content="Invce - Invoice HTML Template">
+    <meta name="robots" content="INDEX,FOLLOW">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&amp;display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/app.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
