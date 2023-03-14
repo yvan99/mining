e2168fc8f1840cb4cf52eb0e69bf302cf98e0116
yvan99 <ishimweyvan90@gmail.com>
2023-03-14 19:52:10 +0200

    feature

---
+++ app/Http/Controllers/AnalyticsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AnalyticsController extends Controller
+{
+    public function adminAnalytics(){
+        $countOrders = DB::table('orders')->count();
+        $countMinerals = DB::table('minerals')->count();
+        $countPartners = DB::table('deliveries')->count();
+
+    }
+}
