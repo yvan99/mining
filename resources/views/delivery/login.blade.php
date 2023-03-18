140a8ab0229c1097dc541b4ad0126c3dbe80013c
yvan99 <ishimweyvan90@gmail.com>
2023-03-18 15:40:28 +0200

    feature

--- resources/views/delivery/login.blade.php
+++ resources/views/delivery/login.blade.php
@@ -11,7 +11,7 @@
                             <div class="card border-1">
                                 <div class="card-header">
                                     <div class="edit-profile__title">
-                                        <h6>Sign in {{ env('APP_NAME') }}</h6>
+                                        <h6>Sign in {{ env('APP_NAME') }} : : Shipping Partner</h6>
                                     </div>
                                 </div>
 
