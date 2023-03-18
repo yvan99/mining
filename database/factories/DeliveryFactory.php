086915059c10dea02dd8d0e70a981d1769b77f6a
yvan99 <ishimweyvan90@gmail.com>
2023-03-18 15:47:56 +0200

    feature

---
+++ database/factories/DeliveryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Delivery;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+class DeliveryFactory extends Factory
+{
+
+    protected $model = Delivery::class;
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('password'), // default password
+            'address' => $this->faker->address,
+            'phone' => $this->faker->phoneNumber,
+            'cli_code' => $this->faker->unique()->randomNumber(),
+        ];
+    }
+}
