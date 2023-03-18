cf9820fa9f27d8937aabfe7ecd4d80487e6fb50a
yvan99 <ishimweyvan90@gmail.com>
2023-03-18 16:47:07 +0200

    feature

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -7,34 +7,13 @@ use Illuminate\Http\Request;
 
 class DeliveryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $deliveries = Delivery::all();
         return view('delivery.index', compact('deliveries'));
     }
     
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $validatedData = $request->validate([
