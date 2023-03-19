306d84ae069b75fdda95fbc2fd553ce9e34eaf51
yvan99 <ishimweyvan90@gmail.com>
2023-03-19 18:58:30 +0200

    feature added

--- resources/views/orders/delivery.blade.php
+++ resources/views/orders/delivery.blade.php
@@ -30,7 +30,7 @@
                             <div class="row justify-content-center">
                                 <div class="col-md-12">
                                     <div class="card">
-                                        <div class="card-header">{{ __('Transit Items Report') }}</div>
+                                        <div class="card-header">{{ __('Assigned Orders Report') }}</div>
                                         <div class="card-body">
                                             @if (session('success'))
                                                 <div class="alert alert-success">
